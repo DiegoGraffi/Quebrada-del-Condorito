0753872a30e8ac9225bbe6702610472adc6be5f3
Diego Graffigna <diegograffigna1@gmail.com>
2023-08-08 20:32:54 -0300

    unofficial site warning

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-white w-full">
       <div className="flex flex-col justify-center items-center md:items-start md:flex-row py-0 md:py-[5rem] max-w-[1200px] mx-auto px-[25px]">
-        <div className="w-full md:w-[65%] border-b py-[2rem] my:py-0 md:border-r md:border-b-0 border-black text-center md:text-left">
+        <div className="w-full md:w-[70%] border-b py-[2rem] my:py-0 md:border-r md:border-b-0 border-black text-center md:text-left md:pr-[25px]">
           <h3 className="font-bold uppercase text-[1rem] md:text-[1.5rem] mb-[1rem]">
             Contacto
           </h3>
@@ -18,27 +18,52 @@ const Footer = () => {
           <p className="w-full md:w-[600px] mb-[1rem] text-[.8rem] md:text-[1rem]">
             <span className="font-[700]">Teléfono: </span> +54 9 3541 532153
           </p>
-          <p className="w-full md:w-[600px] text-[.8rem] md:text-[1rem]">
+          <p className="w-full md:w-[600px] mb-[1rem] text-[.8rem] md:text-[1rem]">
             <span className="font-[700]">Email: </span>{" "}
             quebradadelcondorito@apn.gob.ar
           </p>
+
+          <p className="group w-full md:w-full text-[.8rem] md:text-[1rem] font-[300] bg-verde hover:bg-black transition-all ease-in-out duration-300 p-[1rem] text-white">
+            <span className="font-[700]">Aviso importante: </span> Este sitio
+            web es un proyecto personal de estudio y no representa el sitio web
+            oficial de ninguna entidad. Su contenido es meramente educativo y no
+            posee ningún carácter oficial ni representativo. Para acceder a la
+            fuente oficial, por favor dirígete al sitio web correspondiente de
+            la entidad en cuestión.{" "}
+            <a
+              href="http://www.quebradadelcondorito.ar/index.php"
+              target="_blank"
+              className="border-b border-white group-hover:bg-verde group-hover:p-[.2rem] group-hover:font-[400] group-hover:border-verde transition-all ease-in-out duration-150"
+            >
+              Ingresa aquí
+            </a>
+          </p>
         </div>
 
-        <div className="w-full py-[2rem] my:py-0 md:w-[35%] flex flex-col justify-center items-center md:items-end">
+        <div className="w-full py-[2rem] md:pl-[25px] my:py-0 md:w-[30%] flex flex-col justify-center items-center md:items-end">
           <h3 className="font-bold uppercase text-[1rem] md:text-[1.5rem] mb-[1rem]">
             Seguinos
           </h3>
-          <div className="flex gap-[2rem]">
+          <div className="flex gap-[2rem] w-[200px] justify-between">
             <a href="https://www.facebook.com/PNQuebradaDelCondorito">
-              <IoLogoFacebook size={30} />
+              <IoLogoFacebook
+                size={35}
+                className="hover:text-[#3b5998] hover:scale-125 transition-all ease-in-out duration-200"
+              />
             </a>
 
             <a href="https://twitter.com/turismocba">
-              <IoLogoTwitter size={30} />
+              <IoLogoTwitter
+                size={35}
+                className="hover:text-[#00acee] hover:scale-125 transition-all ease-in-out duration-200"
+              />
             </a>
 
             <a href="https://www.youtube.com/@parquesnacionalesarg">
-              <IoLogoYoutube size={30} />
+              <IoLogoYoutube
+                size={35}
+                className="hover:text-[#ff0000] hover:scale-125 transition-all ease-in-out duration-200"
+              />
             </a>
           </div>
         </div>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -6,10 +6,13 @@ const Hero = () => {
     <section className="min-h-screen relative flex flex-col justify-center items-center">
       <div className="absolute z-40 flex flex-col items-center px-[25px]">
         <h1 className="text-white text-center uppercase text-[2rem] lg:text-[3rem] max-w-[50rem] font-[700] pb-10 border-b border-white">
-          Parque nacional Quebrada del Condorito
+          Parque nacional Quebrada del Condorito <br />
         </h1>
         <p className="text-white uppercase text-center text-[.8rem] lg:text-[1rem] font-[400] pt-10">
           Un encuentro con los condores
+        </p>
+        <p className="text-white uppercase text-center text-[.8rem] lg:text-[1rem] font-[400] pt-5">
+          Sitio web no oficial
         </p>
       </div>
       <div className="h-[100vh] ">
